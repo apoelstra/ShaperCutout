48f3bea55e6ca513ad6c01485430892751810c41
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-07-18 13:15:39 +0000

    insert_if_missing: remove from existing group first
    Also, change move_to_root to just scan obj.InList rather than scanning
    the whole document. Unsure whether the "is this a group that contains
    the object" check is still necessary but it's cheap and now we're only
    doing it for one group, under normal circumstances, rather than the
    whole document.

--- freecad/ShaperCutout/util.py
+++ freecad/ShaperCutout/util.py
@@ -6,7 +6,7 @@ import FreeCADGui as Gui
 
 def move_to_root(obj):
     """Remove obj from any group, placing it at document root."""
-    for o in obj.Document.Objects:
+    for o in obj.InList:
         if hasattr(o, 'Group') and obj in o.Group:
             grp = list(o.Group)
             grp.remove(obj)
@@ -15,6 +15,7 @@ def move_to_root(obj):
 
 def insert_if_missing(container, obj):
     if obj is not None and obj not in container.Group:
+        move_to_root(obj)
         grp = list(container.Group)
         grp.insert(0, obj)
         container.Group = grp
