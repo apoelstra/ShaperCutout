237d31a289dcbd89c2312bdf16de92520933c31d
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-07-26 16:49:07 +0000

    svg export: implement slots

--- freecad/ShaperCutout/command/export_shaper_svg.py
+++ freecad/ShaperCutout/command/export_shaper_svg.py
@@ -293,9 +293,22 @@ def _collect_paths(cutout, dado_groups, drill_holes, mirror=False, addAnchor=Tru
 
     outline_shape = cutout.OutlineSketch.Shape.transformed(xy_matrix)
     outline_wires = outline_shape.Wires
-    outer_wires, inner_wires = _classify_wires(outline_wires)
 
+    # First compute the miter rectangles, which are linked to pre-slot edges
     rect_wires = _miter_rectangles(cutout, xy_matrix)
+    # Then cut any slots, which would relabel the edges, but we don't need the labels anymore.
+    if cutout.Slots:
+        face = Part.makeFace(outline_wires)
+        for slot in cutout.Slots:
+            slot_wire = cutout.Proxy._compute_slot_wire(cutout, slot)
+            if slot_wire is None:
+                continue
+            face = face.cut(Part.Face(slot_wire.transformed(xy_matrix)))
+
+        outline_wires = face.Wires
+    # Then classify the wires as inner/outer, since we won't change shapes anymore.
+    outer_wires, inner_wires = _classify_wires(outline_wires)
+    # *Then* apply the miter rectangles, since their application depends on inner/outer
     outer_wires, inner_wires = _apply_miter_to_wires(outer_wires, inner_wires, rect_wires)
 
     path_elements = []
